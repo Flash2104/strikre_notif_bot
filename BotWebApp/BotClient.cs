578ea6c3c120bd14b8e5073dc0a9ed42df469660
k.khoruzhenko <khoruzhenko.work@gmail.com>
2020-08-26 19:09:14 +0300

    space fix

--- BotWebApp/BotClient.cs
+++ BotWebApp/BotClient.cs
@@ -165,7 +165,7 @@ namespace BotWebApp
             }
             if (!string.IsNullOrWhiteSpace(message.From.FirstName))
             {
-                return message.From.FirstName + message.From.LastName;
+                return message.From.FirstName + " " + message.From.LastName;
             }
 
             return message.Chat.Title;
